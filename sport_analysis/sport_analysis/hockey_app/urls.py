a2f6338fa87492e6ee7d598efa12128cc7237642
banshchikov.alex <banshchikov.alex@outlook.com>
2019-09-25 21:32:46 +0700

    Добавлены маршруты

--- sport_analysis/sport_analysis/hockey_app/urls.py
+++ sport_analysis/sport_analysis/hockey_app/urls.py
@@ -4,5 +4,6 @@ from .views import PlayerStatusView
 app_name = "hockey_app"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('hockey_app/', PlayerStatusView.as_view()),
+    path('hockey_app/player_status/', PlayerStatusView.as_view()),
+    path('hockey_app/player_status/<int:pk>', PlayerStatusView.as_view())
 ]
