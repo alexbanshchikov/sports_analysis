34d5f1487c5062572bead25c32a7b769913a905d
banshchikov.alex <banshchikov.alex@outlook.com>
2019-09-25 21:32:27 +0700

    Разработка API

--- sport_analysis/sport_analysis/hockey_app/serializers.py
+++ sport_analysis/sport_analysis/hockey_app/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import PlayerStatus
 
+
 class PlayerStatusSerializer(serializers.Serializer):
     name = serializers.CharField()
 
     def create(self, validated_data):
         return PlayerStatus.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
--- sport_analysis/sport_analysis/hockey_app/views.py
+++ sport_analysis/sport_analysis/hockey_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import PlayerStatusSerializer
 
 from .models import PlayerStatus
+
 
 class PlayerStatusView(APIView):
     def get(self, request):
@@ -13,11 +15,29 @@ class PlayerStatusView(APIView):
         return Response({"name": serializer.data})
 
     def post(self, request):
-        name = request.data.get('name')
-        serializer = PlayerStatusSerializer(data=name)
-        if serializer.is_valid(raise_exception=True):
-            names = serializer.save()
-        return Response({"success": "names '{}' created successfully".format(names.name)})
+        player_status = request.data.get('player_status')
 
+        serializer = PlayerStatusSerializer(data=player_status)
+        if serializer.is_valid(raise_exception=True):
+            player_statuses = serializer.save()
+        return Response({"success": "names '{}' created successfully".format(player_statuses.name)})
+
+    def put(self, request, pk):
+        saved_player_statuses = get_object_or_404(PlayerStatus.objects.all(), pk=pk)
+        data = request.data.get('player_status')
+        serializer = PlayerStatusSerializer(instance=saved_player_statuses, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            player_status_saved = serializer.save()
+        return Response({
+            "success": "Player_status '{}' updated successfully".format(player_status_saved.name)
+        })
+
+    def delete(self, request, pk):
+        # Get object with this pk
+        player_status = get_object_or_404(PlayerStatus.objects.all(), pk=pk)
+        player_status.delete()
+        return Response({
+            "message": "Player status with id `{}` has been deleted.".format(pk)
+        }, status=204)
 
 # Create your views here.
